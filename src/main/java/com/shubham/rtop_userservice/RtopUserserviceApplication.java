4f172891e473aff53cfcbdd86b9cb3c6efeba2e3
Shubham <shubhamsriv98@gmail.com>
2026-02-27 01:15:01 +0530

    Initial commit - Spring Boot microservices project

---
+++ src/main/java/com/shubham/rtop_userservice/RtopUserserviceApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.rtop_userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtopUserserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtopUserserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubham/rtop_userservice/RtopUserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.rtop_userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtopUserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
